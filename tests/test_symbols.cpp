bcf46ef30b169ba29455d35e4858ba753ca99e8f
mixer <sergey594m@gmail.com>
2025-08-26 09:04:54 +0300

    add tests

--- tests/test_symbols.cpp
+++ tests/test_symbols.cpp
@@ -35,3 +35,37 @@ TEST(EnvTest, FindsInOuterScope) {
   ASSERT_NE(found, nullptr);
   EXPECT_EQ(found->type->name, "float");
 }
+
+TEST(TypeTest, MaxTypeBothEqual) {
+  auto resultBothInt = Type::max(Type::Int, Type::Int);
+  auto resultBothFloat = Type::max(Type::Float, Type::Float);
+  auto resultBothChar = Type::max(Type::Char, Type::Char);
+
+  EXPECT_EQ(resultBothInt, Type::Int);
+  EXPECT_EQ(resultBothFloat, Type::Float);
+  EXPECT_EQ(resultBothChar, Type::Char);
+}
+
+TEST(TypeTest, MaxTypeBothNotEqual) {
+  auto resultBoolFloat = Type::max(Type::Bool, Type::Float);
+  auto resultFloatBool = Type::max(Type::Float, Type::Bool);
+  auto resultBoolInt = Type::max(Type::Bool, Type::Int);
+  auto resultIntBool = Type::max(Type::Int, Type::Bool);
+  auto resultCharInt = Type::max(Type::Char, Type::Int);
+  auto resultIntChar = Type::max(Type::Int, Type::Char);
+  auto resultCharFloat = Type::max(Type::Char, Type::Float);
+  auto resultFloatChar = Type::max(Type::Float, Type::Char);
+  auto resultIntFloat = Type::max(Type::Int, Type::Float);
+  auto resultFloatInt = Type::max(Type::Float, Type::Int);
+
+  EXPECT_EQ(resultBoolFloat, nullptr);
+  EXPECT_EQ(resultFloatBool, nullptr);
+  EXPECT_EQ(resultBoolInt, nullptr);
+  EXPECT_EQ(resultIntBool, nullptr);
+  EXPECT_EQ(resultCharInt, Type::Int);
+  EXPECT_EQ(resultIntChar, Type::Int);
+  EXPECT_EQ(resultCharFloat, Type::Float);
+  EXPECT_EQ(resultFloatChar, Type::Float);
+  EXPECT_EQ(resultIntFloat, Type::Float);
+  EXPECT_EQ(resultFloatInt, Type::Float);
+}
