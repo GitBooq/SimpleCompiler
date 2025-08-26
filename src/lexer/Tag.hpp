31f8d55c1aa8736d67bffdec629b37c5672d38f9
mixer <sergey594m@gmail.com>
2025-08-26 11:35:16 +0300

    refactor enum class Tag, add additional tags

--- src/lexer/Tag.hpp
+++ src/lexer/Tag.hpp
@@ -8,29 +8,41 @@ namespace lexer {
 
 /**
  * @enum Tag
- * @brief Symbolic codes for special tokens, distinct from single ASCII
+ * @brief Symbolic codes for tokens
  * characters.
  */
 enum class Tag : int {
-  ID = 256, /**< Identifier */
-  NUM,      /**< Integer literal */
-  REAL,     /**< Floating-point literal */
-  TRUE_,    /**< Boolean literal true */
-  FALSE_,   /**< Boolean literal false */
-  IF,       /**< Keyword if */
-  ELSE,     /**< Keyword else */
-  WHILE,    /**< Keyword while */
-  DO,       /**< Keyword do */
-  BREAK,    /**< Keyword break */
-  BASIC,    /**< Basic type (int, float, bool) */
-  OR,       /**< Logical operator || */
-  AND,      /**< Logical operator && */
-  EQ,       /**< Comparison operator == */
-  NE,       /**< Comparison operator != */
-  LE,       /**< Comparison operator <= */
-  GE,       /**< Comparison operator >= */
-  INDEX,    /**< Array indexing */
-  MINUS,    /**< Unary minus */
-  TEMP      /**< Temporary variable */
+  ID,     /**< Identifier */
+  NUM,    /**< Integer literal */
+  REAL,   /**< Floating-point literal */
+  TRUE_,  /**< Boolean literal true */
+  FALSE_, /**< Boolean literal false */
+  IF,     /**< Keyword if */
+  ELSE,   /**< Keyword else */
+  WHILE,  /**< Keyword while */
+  DO,     /**< Keyword do */
+  BREAK,  /**< Keyword break */
+  BASIC,  /**< Basic type (int, float, bool) */
+  OR,     /**< Logical operator || */
+  AND,    /**< Logical operator && */
+  EQ,     /**< Comparison operator == */
+  NE,     /**< Comparison operator != */
+  LE,     /**< Comparison operator <= */
+  GE,     /**< Comparison operator >= */
+  INDEX,  /**< Array indexing */
+  MINUS,  /**< Unary minus */
+  TEMP,   /**< Temporary variable */
+
+  OP_PLUS,  /**< Bin operator '+' */
+  OP_MINUS, /**< Bin operator '-' */
+  OP_DIV,   /**< Bin operator '/' */
+  OP_MUL,   /**< Bin operator '*' */
+
+  bitAND,   /**< Bit operator '&' */
+  bitOR,    /**< Bit operator '|' */
+  ASSIGN,   /**< Assign operator '=' */
+  UnaryNOT, /**< Bit not operator '!' */
+  LESS,     /**< Relation less operator '<' */
+  GREATER   /**< Relation greater operator '>' */
 };
 }  // namespace lexer
